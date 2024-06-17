3e2efdd35986fe81df0219452c6cc9fb89b44fce
roswell pando <184652@unsaac.edu.pe>
2024-06-17 11:04:59 -0500

    first part tutorial finished

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [preact()]
+} // for use rss package
+// site: "url.deployed"
+);
